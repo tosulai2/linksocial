263d4613e187a558c2665640a67766d67855ff0e
Taofik S <taofik.sulaiman@gmail.com>
2018-11-13 18:12:04 -0600

    test commit 3

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -42,7 +42,7 @@ export default class HomeScreen extends React.Component {
             </View>
 
             <Text style={styles.getStartedText}>
-              BITCH!!! Change this text and your app will automatically reload.
+              BITCH!! Change this text and your app will automatically reload.
             </Text>
           </View>
 
